b5b7b850f474ee9f067da674b4ffd01051ba5041
Aaron Kolker <akolker2@me.com>
2013-03-15 22:05:46 -0400

    asdf

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Harriton High School TSA</title>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+
+</head>
+
+
+
+
+
+<body>
+
+
+<body background="images/wood.jpg">
+
+
+<div id="header"></div>
+
+
+<div id="content">
+
+<div id="content_block">Content for id &quot;content_block&quot; Goes Here</div>
+
+  <div id="sidebar">
+  <ul class="list">
+  <li><a href="index.php"><em>HHS</em>Home</a></li>
+  <li><a href="Tech Ed/TechEdhome.html"><em>HHS</em>Technology Education</a></li>
+  <li><a href="TSA/TSAhome.html"><em>HHS</em>Harriton TSA</a></li>
+  <li><a href="Design Brief/DesignBriefhome.html"><em>HHS</em>Design Brief</a></li>
+  </ul>
+  </div>
+</div>
+<div id="footer">Content for id &quot;footer&quot; Goes Here</div>
+<p>&nbsp;</p>
+
+
+
+</body>
+
+</html>
